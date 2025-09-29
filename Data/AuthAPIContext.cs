a849a54fd342e753bb5de424ad6ee85a896c6a99
RESMARKZ <dacurawatmark@gmail.com>
2025-09-29 11:14:45 +0800

    Add project files.

---
+++ Data/AuthAPIContext.cs
@@ -0,0 +1,12 @@
+﻿using AuthApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthApi.Data
+{
+    public class AuthAPIContext: DbContext
+    {
+        public AuthAPIContext(DbContextOptions<AuthAPIContext> options): base(options){}
+
+        DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Role { get; set; } = null!;
+    }
+}
